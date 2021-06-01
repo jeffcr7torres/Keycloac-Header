f2b190de5d8df98e5c0a7396927d6ef63c1c53fa
Jefferson Patricio Torres Bastidas <ptorres@ec.krugercorp.com>
2021-06-01 08:59:09 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+import {InicializarKeycloack} from './AppInit';
+import {AuthService} from './services/authentication/auth.service'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    KeycloakAngularModule
   ],
-  providers: [],
+  providers: [KeycloakService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: InicializarKeycloack,
+      multi: true,
+      deps:[KeycloakService]
+    }, AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
